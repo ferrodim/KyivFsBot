84c9b30540e7531ff3196f52f84c71c835ec456a
ferrodim <ferrodim.ua@gmail.dom>
2019-04-06 01:56:16 +0300

    fix: codestyle

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -5,17 +5,18 @@ from PIL import Image
 from time import sleep
 from image_process import parse_image
 
+
 def on_message(channel, method_frame, header_frame, body):
     LOG.info(' <= %s', body)
     msg = json.loads(body)
     img = Image.open('/Screens/' + msg['img'])
-    parseResult = parse_image(img)
-    parseResult['msgid'] = msg['msgid']
-    parseResult['chatid'] = msg['chatid']
-    parseResult['datakey'] = msg['datakey']
-    parseResult['agentname'] = msg['agentname']
-    LOG.info(' => ' + json.dumps(parseResult))
-    channel.basic_publish('', 'results', json.dumps(parseResult))
+    parse_result = parse_image(img)
+    parse_result['msgid'] = msg['msgid']
+    parse_result['chatid'] = msg['chatid']
+    parse_result['datakey'] = msg['datakey']
+    parse_result['agentname'] = msg['agentname']
+    LOG.info(' => ' + json.dumps(parse_result))
+    channel.basic_publish('', 'results', json.dumps(parse_result))
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 
