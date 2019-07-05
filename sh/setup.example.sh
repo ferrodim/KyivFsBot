466583a5f3ade6653b298badfe435b05d9dd1e47
ferrodim <ferrodim.ua@gmail.dom>
2019-07-06 01:13:49 +0300

    feature: setup example updated

--- sh/setup.example.sh
+++ sh/setup.example.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-# Use this script as hint, how to setup bot on fresh Ubuntu 16.04 environment 
+# Use this script as hint, how to setup bot on fresh Ubuntu 18.04 environment
 sudo -s
 apt update
-apt dist-upgrade
+# apt dist-upgrade
 apt upgrade
 apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-
 apt install docker-ce docker-ce-cli containerd.io docker-compose git
 git clone https://github.com/ferrodim/KyivFsBot.git
 cd KyivFsBot/
+cp config.sample.py config.py
 vi config.py
+# change THREAD_NUM in docker-compose.yml if you with to use more/less than 5 CPU cores for image decoding
 docker-compose up -d
