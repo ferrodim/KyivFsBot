6637b2e4200cefb0610df221db4e4b7d0f68cf51
ferrodim <ferrodim.ua@gmail.dom>
2019-07-15 19:57:56 +0300

    feature: bit faster installing

--- sh/setup.example.sh
+++ sh/setup.example.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # Use this script as hint, how to setup bot on fresh Ubuntu 18.04 environment
 sudo -s
-apt update
-# apt dist-upgrade
-apt upgrade
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt update -y
+apt upgrade -y
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose git
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose git
 git clone https://github.com/ferrodim/KyivFsBot.git
 cd KyivFsBot/
 cp config.sample.py config.py
