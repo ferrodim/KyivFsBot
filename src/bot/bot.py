7d8fa81f82192a0d5e607a276c1d077639d990bc
ferrodim <ferrodim.ua@gmail.dom>
2019-09-30 10:22:44 +0300

    feature: /agents - output reformatted

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -269,8 +269,8 @@ def cmd_agents(message):
         txt += "\n"
         nick = data["counters"][agentname].get("Nick", "-")
         fraction = data["counters"][agentname].get("fraction", "-")  # \U0001F438 Frog  \U0001F41F Fish
-        img = fraction_icon(fraction) or '-'
-        txt += "@%s / `%s` / %s" % (agentname.replace('_', '\\_'), nick.replace('_', '\\_'), img)
+        img = fraction_icon(fraction)
+        txt += "%s@%s / `%s`" % (img, agentname.replace('_', '\\_'), nick.replace('_', '\\_'))
     bot.send_message(message.chat.id, txt, parse_mode="Markdown")
 
 
