4794df1e075a06cbdea9901a7da2791dce9bdfb3
ferrodim <ferrodim.ua@gmail.dom>
2019-11-07 01:50:15 +0200

    feature: node.js + rabbit hello-world

--- src/logger/app.js
+++ src/logger/app.js
@@ -1,3 +1,19 @@
+let connect = require('amqplib').connect(process.env.RABBIT_URL);
+let queueName = 'logger';
+
+connect.then(con => {
+    console.log('connection ready');
+    return con.createChannel();
+}).then(ch => {
+    return ch.assertQueue(queueName).then(function(ok) {
+        return ch.consume(queueName, function(msg) {
+            if (msg !== null) {
+                console.log(msg.content.toString());
+                ch.ack(msg);
+            }
+        });
+    });
+});
 
 console.log('logger init');
 
