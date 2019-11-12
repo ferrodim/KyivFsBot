39b77873eceee367825dc19a9163618f43058cd1
ferrodim <ferrodim.ua@gmail.dom>
2019-11-12 03:10:26 +0200

    feature: script for faster .po files sync

--- sh/regenerate_localization.sh
+++ sh/regenerate_localization.sh
@@ -1,1 +1,4 @@
 xgettext -o i18n/messages.pot --from-code utf-8 src/bot/bot.py src/decoder/decoder.py src/translator/app.js
+msgmerge -U i18n/en.po i18n/messages.pot --backup=off
+msgmerge -U i18n/ua.po i18n/messages.pot --backup=off
+msgmerge -U i18n/ru.po i18n/messages.pot --backup=off
