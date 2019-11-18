af44b7c82991d18a2968ee43c2d00a2c2fe5c859
ferrodim <ferrodim.ua@gmail.dom>
2019-11-18 02:26:41 +0200

    fix: localization for all microservices

--- sh/regenerate_localization.sh
+++ sh/regenerate_localization.sh
@@ -1,4 +1,4 @@
-xgettext -o i18n/messages.pot --from-code utf-8 src/bot/bot.py src/decoder/decoder.py src/translator/app.js
+xgettext -o i18n/messages.pot --from-code utf-8 src/*/*.py src/*/*.js
 msgmerge -U i18n/en.po i18n/messages.pot --backup=off
 msgmerge -U i18n/ua.po i18n/messages.pot --backup=off
 msgmerge -U i18n/ru.po i18n/messages.pot --backup=off
