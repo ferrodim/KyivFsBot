8a153c8049cfb729528228903229e378791767f0
ferrodim <ferrodim.ua@gmail.dom>
2019-11-19 02:20:47 +0200

    housekeeping: rabbit incapsulated into framework

--- src/store/app.js
+++ src/store/app.js
@@ -1,39 +1,28 @@
-let connect = require('amqplib').connect(process.env.RABBIT_URL);
-let queueName = 'store';
-let {mongo} = require('./framework');
+const {mongo, rabbit, _} = require('./framework');
+const APP_NAME = 'store';
 
-mongo.configure({url: process.env.MONGO_URL});
-
-connect.then(con => {
-    console.log('connection ready');
-    return con.createChannel();
-}).then(async ch => {
-    await ch.assertQueue(queueName);
-    await ch.bindQueue(queueName, 'topic', 'core.messageIn');
-    await mongo.ready;
-    ch.consume(queueName, function(msg) {
-        if (msg !== null) {
-            let event = JSON.parse(msg.content.toString());
-            console.log('{Rabbit} <= ' + JSON.stringify(event));
-            if (event.event === 'core.messageIn'){
-                if (event.text === '/store_info'){
-                    sendTxt(ch, event.chatid, _('Here will be info about storage'), []);
-                } else if (event.text === '/store_increment'){
-                    storeIncrement(ch, event);
-                } else if (event.text === '/store_read'){
-                    storeRead(ch, event);
-                } else if (event.text === '/ping'){
-                    sendTxt(ch, event.chatid, _('Pong from %s'), ["store"]);
-                }
-            } else {
-                console.log('unknown event', event);
-            }
-            ch.ack(msg);
+Promise.all([
+    mongo.configure({
+        url: process.env.MONGO_URL
+    }),
+    rabbit.configure({
+        url: process.env.RABBIT_URL,
+    }),
+]).then(async () => {
+    await rabbit.bind(APP_NAME, 'core.messageIn', function(event) {
+        if (event.text === '/store_info'){
+            sendTxt(event.chatid, _('Here will be info about storage'), []);
+        } else if (event.text === '/store_increment'){
+            storeIncrement(event);
+        } else if (event.text === '/store_read'){
+            storeRead(event);
+        } else if (event.text === '/ping'){
+            sendTxt(event.chatid, _('Pong from %s'), ["store"]);
         }
     });
 });
 
-async function storeIncrement(ch, event){
+async function storeIncrement(event){
     await mongo.collection('demo').findOneAndUpdate({
             id: 'counter',
         },{
@@ -42,16 +31,16 @@ async function storeIncrement(ch, event){
             upsert: true,
         }
     );
-    sendTxt(ch, event.chatid, _('Incremented'));
+    sendTxt(event.chatid, _('Incremented'));
 }
 
-async function storeRead(ch, event){
+async function storeRead(event){
     let counter = await mongo.collection('demo').findOne({id: 'counter'});
     let value = counter ? counter.val : 0;
-    sendTxt(ch, event.chatid, _('Counter in db: %s'), [value]);
+    sendTxt(event.chatid, _('Counter in db: %s'), [value]);
 }
 
-function sendTxt(ch, chatId, text, placeholders){
+function sendTxt(chatId, text, placeholders){
     let outcomeEvent = {
         event: 'call.translateAndSend',
         args: {
@@ -60,11 +49,6 @@ function sendTxt(ch, chatId, text, placeholders){
             placeholders: placeholders
         }
     };
-    let outStr = JSON.stringify(outcomeEvent);
-    console.log('{Rabbit} => ' + outStr);
-    ch.publish('topic', outcomeEvent.event, Buffer.from(outStr, 'utf8'));
+    rabbit.emit(outcomeEvent);
 }
 
-function _(msg){
-    return msg;
-}
--- src/store/framework.js
+++ src/store/framework.js
@@ -1,5 +1,4 @@
 
-
 function MongoService(){
     this._db = null;
     this.ready = new Promise((accept, reject) =>  {
@@ -19,7 +18,39 @@ MongoService.prototype.collection = function(name){
     return this._db.collection(name);
 };
 
+function RabbitService(){
+    this.channel = null;
+    this.ready = new Promise((accept, reject) =>  {
+        this.configure = async function(args){
+            let connect = await require('amqplib').connect(args.url);
+            this.channel = await connect.createChannel();
+            accept();
+            return this.ready;
+        };
+    });
+}
+
+RabbitService.prototype.bind = async function(queueName, routeKey, callback){
+    await this.channel.assertQueue(queueName);
+    await this.channel.bindQueue(queueName, 'topic', routeKey);
+    this.channel.consume(queueName, async msg => {
+        if (msg){
+            let event = JSON.parse(msg.content.toString());
+            console.log('{Rabbit} <= ' + JSON.stringify(event));
+            await callback(event);
+        }
+        this.channel.ack(msg);
+    });
+};
+
+RabbitService.prototype.emit = function(event){
+    let outStr = JSON.stringify(event);
+    console.log('{Rabbit} => ' + outStr);
+    this.channel.publish('topic', event.event, Buffer.from(outStr, 'utf8'));
+};
 
 module.exports = {
-    mongo: new MongoService()
+    mongo: new MongoService(),
+    rabbit: new RabbitService(),
+    _: function (msg){return msg},
 };
