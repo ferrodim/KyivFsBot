7145cd5cc05dd83a7888e724cbd8a6022736bc93
ferrodim <ferrodim.ua@gmail.dom>
2019-11-21 02:04:40 +0200

    feature: shared code - framework.js for all node.js microservices

---
+++ src/framework.js
@@ -0,0 +1,68 @@
+
+const CONNECT_TIMEOUT = 5000;
+
+function MongoService(){
+    this._db = null;
+    this.ready = new Promise((accept, reject) =>  {
+        this.configure = async function(args){
+            let connect_timer = setTimeout(()=>{
+                reject("Mongo connection timeout, after " + CONNECT_TIMEOUT + 'ms wait');
+            }, CONNECT_TIMEOUT);
+
+            require('mongodb').MongoClient
+                .connect(args.url, {useUnifiedTopology: true})
+                .then(client => {
+                    clearTimeout(connect_timer);
+                    console.log("Connected successfully to db server");
+                    this._db = client.db();
+                    accept();
+                }, err => {
+                    clearTimeout(connect_timer);
+                    reject(err);
+                });
+
+            return this.ready;
+        };
+    });
+}
+
+MongoService.prototype.collection = function(name){
+    return this._db.collection(name);
+};
+
+function RabbitService(){
+    this.channel = null;
+    this.ready = new Promise((accept, reject) =>  {
+        this.configure = async function(args){
+            let connect = await require('amqplib').connect(args.url);
+            this.channel = await connect.createChannel();
+            accept();
+            return this.ready;
+        };
+    });
+}
+
+RabbitService.prototype.bind = async function(queueName, routeKey, callback){
+    await this.channel.assertQueue(queueName);
+    await this.channel.bindQueue(queueName, 'topic', routeKey);
+    this.channel.consume(queueName, async msg => {
+        if (msg){
+            let event = JSON.parse(msg.content.toString());
+            console.log('{Rabbit} <= ' + JSON.stringify(event));
+            await callback(event);
+        }
+        this.channel.ack(msg);
+    });
+};
+
+RabbitService.prototype.emit = function(event){
+    let outStr = JSON.stringify(event);
+    console.log('{Rabbit} => ' + outStr);
+    this.channel.publish('topic', event.event, Buffer.from(outStr, 'utf8'));
+};
+
+module.exports = {
+    mongo: new MongoService(),
+    rabbit: new RabbitService(),
+    _:_=>_,
+};
