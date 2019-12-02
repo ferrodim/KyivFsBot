13f8e17808777b29d9c6246c698df91bc6606310
ferrodim <ferrodim.ua@gmail.dom>
2019-12-02 03:57:18 +0200

    fix: po files have too much modified lines every rebuild

--- sh/regenerate_localization.sh
+++ sh/regenerate_localization.sh
@@ -1,4 +1,4 @@
-xgettext -o i18n/messages.pot --from-code utf-8 src/*/*.py src/*/*.js
+xgettext -o i18n/messages.pot --no-location --from-code utf-8 src/*/*.py src/*/*.js
 msgmerge -U i18n/en.po i18n/messages.pot --backup=off
 msgmerge -U i18n/ua.po i18n/messages.pot --backup=off
 msgmerge -U i18n/ru.po i18n/messages.pot --backup=off
