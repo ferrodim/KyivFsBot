01b3da127683e32ac30b9c7bf951d59ba7dd3d12
ferrodim <ferrodim.ua@gmail.dom>
2019-12-29 03:56:19 +0200

    feature: /help extended

---
+++ src/store/handlers/cmd_help.js
@@ -0,0 +1,17 @@
+const {_} = require('flatground');
+const sendText = require('../sendText');
+
+module.exports = async function (event){
+    if (!event.isAdmin){
+        return;
+    }
+    let txt = "/admin_list - Admin list \n";
+    txt += "/admin_add <tgnick> - Add admin \n";
+    txt += "/admin_remove <tgnick> - Remove admin\n";
+    txt += "/city_start_time <time> - Used inside welcome notification\n";
+    txt += "/city_end_time <time> - Used inside welcome notification\n";
+    txt += "/modes_list - Configure modes for /me, /result \n";
+    txt += "/modes_add <mode> - Configure modes for /me, /result \n";
+    txt += "/modes_remove <mode> - Configure modes for /me, /result\n";
+    sendText(event.chatid, txt);
+};
