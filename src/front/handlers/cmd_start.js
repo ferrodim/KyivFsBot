0176f318c94f06c18bc86bf6e28c4b03223c6981
ferrodim <ferrodim.ua@gmail.com>
2020-12-01 05:19:25 +0200

    clearing

--- src/front/handlers/cmd_start.js
+++ src/front/handlers/cmd_start.js
@@ -3,8 +3,8 @@ const sendText = require('../sendText');
 
 module.exports = async function (event){
     let city = event.city;
-    let startTime = city.startTime || 'not filled';
-    let endTime = city.endTime || 'not filled';
+    let startTime = city.startTime || '??';
+    let endTime = city.endTime || '??';
     // let modes = city.modes || [];
     let template = _('Welcome, export me your start data at *%s*, and finish data at *%s*. Also use [google form](%s). Write /help for more info');
     sendText(event.chatid, template, [startTime, endTime, city.statUrl], true);
