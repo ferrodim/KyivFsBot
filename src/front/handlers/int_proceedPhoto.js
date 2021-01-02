7fdd8e5540ad783837d61399dd2a9c3125fea6ac
ferrodim <ferrodim.ua@gmail.com>
2021-01-02 03:13:52 +0200

    feature: simple logs

--- src/front/handlers/int_proceedPhoto.js
+++ src/front/handlers/int_proceedPhoto.js
@@ -1,4 +1,4 @@
-const {_} = require('flatground');
+const {mongo, _} = require('flatground');
 const bot = require('../services/bot');
 const sendText = require('../sendText');
 const env = process.env;
@@ -13,5 +13,14 @@ module.exports = async function proceedPhoto(msg){
         return;
     }
     await bot.forwardMessage(env.IMG_CHAT, msg.chat.id, msg.message_id);
+
+    await mongo.collection('log').insert({
+        type: 'imageForwarded',
+        chatId: msg.chat.id,
+        msgId: msg.message_id,
+        date: Date.now(),
+        caption: msg.caption,
+    });
+
     sendText(msg.chat.id, _('Image forwarded'));
 }
